a6b4da70d34679db3ff18e1f44f41832326a94d9
Sam Anthony <sam@samanthony.xyz>
2024-11-20 10:07:19 -0500

    stash IL

--- src/main/java/derms/net/runicast/State.java
+++
@@ -1,5 +0,0 @@
-package derms.net.runicast;
-
-public enum State {
-    closed, syncer, syncee, established, listening, closing, opening
-}
--- src/main/java/derms/net/runicast/Type.java
+++
@@ -1,5 +0,0 @@
-package derms.net.runicast;
-
-public enum Type {
-    sync, data, dataquery, ack, query, state, close
-}
