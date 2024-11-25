50d1399fc568f8fdd84593d293761c9d44fc6d10
Sam Anthony <sam@samanthony.xyz>
2024-11-25 10:27:05 -0500

    request and response stubs

---
+++ src/main/java/derms/Request.java
@@ -0,0 +1,13 @@
+package derms;
+
+import derms.net.MessagePayload;
+
+// TODO
+public class Request implements MessagePayload {
+
+    @Override
+    public int hash() {
+        // TODO
+        return -1;
+    }
+}
---
+++ src/main/java/derms/Response.java
@@ -0,0 +1,13 @@
+package derms;
+
+import derms.net.MessagePayload;
+
+// TODO
+public class Response implements MessagePayload {
+
+    @Override
+    public int hash() {
+        // TODO
+        return  -1;
+    }
+}
