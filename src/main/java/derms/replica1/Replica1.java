79837dd95a90891943659194c43f732a364ad9ff
ShazaAli <shazamamdouh@aucegypt.edu>
2024-12-03 07:29:45 -0500

    Replica 1 implement all functions

--- src/main/java/derms/replica1/Replica1.java
+++ src/main/java/derms/replica1/Replica1.java
@@ -21,6 +21,9 @@ public class Replica1 implements Replica {
     private final Logger log;
     private final InetAddress localAddr;
     private final ResponderClient responderClient;
+    private final CoordinatorClient coordinatorClient;
+    private final String responderClientID = "MTL";
+    private final String coordinatorClientID = "MTLC1111";
     private final ReplicaManager replicaManager;
 
     public Replica1(ReplicaManager replicaManager) {
@@ -31,7 +34,8 @@ public class Replica1 implements Replica {
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-        responderClient = new ResponderClient("MTL");
+        responderClient = new ResponderClient(responderClientID);
+        coordinatorClient = new CoordinatorClient(coordinatorClientID);
         try {
             this.log = DermsLogger.getLogger(getClass());
         } catch (IOException e) {
@@ -54,12 +58,41 @@ public class Replica1 implements Replica {
 
     @Override
     public void processRequest(Request request) {
-        String status = responderClient.addResource(
-                request.getResourceID(),
-                request.getResourceType(),
-                request.getDuration()
-        );
-        Response response = new Response(request.getSequenceNumber(), status);
+        log.info(request.toString());
+
+        String status = "";
+        try {
+            switch (request.getFunction()) {
+                case "addResource":
+                    status = responderClient.addResource(request.getResourceID(), request.getResourceType(), request.getDuration());
+                    break;
+                case "removeResource":
+                    status = responderClient.removeResource(request.getResourceID(), request.getDuration());
+                    break;
+                case "listResourceAvailability":
+                    status = String.join(",", responderClient.listResourceAvailability(request.getResourceType()));
+                    break;
+                case "requestResource":
+                    status = coordinatorClient.requestResource(coordinatorClientID, request.getResourceID(), request.getDuration());
+                    break;
+                case "findResource":
+                    status = String.join(",", coordinatorClient.findResource(coordinatorClientID, request.getResourceType()));
+                    break;
+                case "returnResource":
+                    status = coordinatorClient.returnResource(coordinatorClientID, request.getResourceID());
+                    break;
+                case "swapResource":
+                    status = coordinatorClient.swapResource(coordinatorClientID, request.getOldResourceID(), request.getOldResourceType(), request.getResourceID(), request.getResourceType());
+                    break;
+                default:
+                    status = "Failure: unknown function '" + request.getFunction() + "'";
+            }
+        } catch (Exception e) {
+            log.warning(e.getMessage());
+            status = "Failure: " + request.getFunction() + ": " + e.getMessage();
+        }
+
+        Response response = new Response(request, replicaManager.getReplicaId(), status, false); // TODO: isSuccess flag
         log.info("Processed request " + request + "; response: " + response);
         replicaManager.sendResponseToFE(response);
     }
