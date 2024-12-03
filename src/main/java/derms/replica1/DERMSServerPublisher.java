92bf77c700873f2a014ad09b50790c2a1032ebd5
BMatajsz <90217645+BMatajsz@users.noreply.github.com>
2024-12-03 11:25:43 -0500

    Conflict1

--- src/main/java/derms/replica1/DERMSServerPublisher.java
+++ src/main/java/derms/replica1/DERMSServerPublisher.java
@@ -23,5 +23,13 @@ public class DERMSServerPublisher {
                 System.out.println("DERMS Server is stopped.");
             }
         }
+//        try {
+//            Endpoint.publish("http://localhost:8387/ws/derms", new DERMSServer("MTL"));
+//            Endpoint.publish("http://localhost:8081/ws/derms", new DERMSServer("QUE"));
+//            Endpoint.publish("http://localhost:8082/ws/derms", new DERMSServer("SHE"));
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("DERMS Web Service is published at http://localhost:8387/ws/derms");
     }
 }
